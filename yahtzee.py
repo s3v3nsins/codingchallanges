27e316357a00cc99a1d0aa00080b3cbbef63db97
s3v3nsins <71485000+s3v3nsins@users.noreply.github.com>
2020-10-10 15:13:39 -0700

    Update yahtzee.py

--- yahtzee.py
+++ yahtzee.py
@@ -2,7 +2,7 @@
 # Write Python 3 code in this online editor and run it.
 import operator
 
-// Space O(n)  and time O(n)
+# Space O(n)  and time O(n)
 def yahtzee(dice_roll):
     dmap = {key:0 for key in range(1,7)}
     for num in dice_roll:
@@ -10,9 +10,9 @@ def yahtzee(dice_roll):
     return max(dmap.items(), key=operator.itemgetter(1))[1]
 print(yahtzee([2, 3, 5, 5, 6]))
 
-// Space O(1)  and time O(n)
+# Space O(1)  and time O(n)
 def yahtzee2(dice_roll):
-    // current max, previos max
+    # current max, previos max
     cm, pm = 0, 0
     index = 0
     for num in dice_roll:
@@ -26,6 +26,24 @@ def yahtzee2(dice_roll):
         elif num != dice_roll[index-1] and cm < pm:
             cm = num
         index += 1
-     // return max
+    # return max
     return max(pm, cm)
 print(yahtzee2([2, 3, 5, 5, 6]))
+
+
+from collections import defaultdict
+import operator, time
+
+# Space: O(n) & time O(n)
+def yahtzee_bonus(file_name):
+    f = open(file_name, 'r') 
+    lines = f.readlines() 
+    dmap = defaultdict()
+    for line in lines:
+      line = line.strip()
+      if line in dmap.keys(): 
+        dmap[line.strip()] += line.strip()
+      else:
+        dmap[line.strip()] = line.strip()
+    return max(dmap.items(), key=operator.itemgetter(1))[1]
+print(yahtzee_bonus("yahtzee-upper-1.txt"))
